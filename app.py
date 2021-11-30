8280c94a6b7c258ca6d68716bf9b6c1e2f622d78
matty255 <matt14507@gmail.com>
2021-11-30 19:30:31 +0900

    페이지연결

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print ('hello')
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__, template_folder='templates')
+
+
+
+@app.route('/')
+def home():
+        return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/classlist')
+def classlist():
+    return render_template('classList.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+@app.route('/guestbook')
+def guestbook():
+    return render_template('guestBook.html')
+
+
+
+@app.errorhandler(404) 
+def page_not_found(error):
+    return render_template('404notfound.html')
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/move.js
@@ -0,0 +1,25 @@
+
+        $(document).ready(function () {
+
+        });
+
+        function home() {
+            window.location.href = '/';
+        };
+
+        function detail() {
+            window.location.href = '/detail';
+        };
+
+        function about() {
+            window.location.href = '/about';
+        };
+
+        function classlist() {
+            window.location.href = '/classlist';
+        };
+
+        function guestbook() {
+            window.location.href = '/guestbook';
+        };
+        
